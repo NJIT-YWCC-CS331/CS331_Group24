7c747f5d792fbc3a13ed4f6d0077d56198efafaa
PabloVega7789 <pv46@njit.edu>
2025-12-11 00:04:44 -0500

    Create user_hub.php

---
+++ user_hub.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>User Hub</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>User Hub</h1>
+    <div class="img_wrapper">
+        <div class="option">
+            <h3>Your Profile:</h3>
+            <a href="profile.php">
+                <img src="https://t3.ftcdn.net/jpg/06/19/26/46/360_F_619264680_x2PBdGLF54sFe7kTBtAvZnPyXgvaRw0Y.jpg" 
+                     alt="Profile" class="icon">
+            </a>
+        </div>
+        <div class="option">
+            <h3>Search Cars:</h3>
+            <a href="search_car.php">
+                <img src="https://i.etsystatic.com/10313448/r/il/ac2efd/3176501141/il_1080xN.3176501141_tomi.jpg" 
+                     alt="Search" class="icon">
+            </a>
+        </div>
+        <div class="option">
+            <h3>Reserve a Car:</h3>
+            <a href="reserve.php">
+                <img src="https://images.vexels.com/media/users/3/145851/isolated/preview/c026c871da318362b6f48654b49c4896-smart-car-front-view-silhouette.png"
+                     alt="Reservation" class="icon">
+            </a>
+        </div>
+    </div>
+    <a href="logout.php">Log Out</a>
+</div>
+
+</body>
+</html>
