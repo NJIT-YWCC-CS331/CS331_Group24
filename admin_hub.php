0b9812126a711c5a8185e2e1e3ca698204489ab4
PabloVega7789 <pv46@njit.edu>
2025-12-10 23:59:19 -0500

    Create admin_hub.php

---
+++ admin_hub.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Select Login Type</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>Admin Hub</h1>
+    <div class="img_wrapper">
+        <div class="option">
+        <h3>User List: </h3>
+            <a href="user_list.php">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" alt="Profile" class="icon">
+            </a>
+        </div>
+
+        <div class="option">
+            <h3>Rental List: </h3>
+            <a href="registration_list.php">
+                <img src="https://media.istockphoto.com/id/1823333678/vector/car-icon-black-contour-linear-silhouette-editable-strokes-side-view-vector-simple-flat.jpg?s=612x612&w=0&k=20&c=Ik343kXklVck5usIk6r8GrkeeASmADcjb-lWtVNHw-Y=" alt="Car" class="icon">
+            </a>
+        </div>
+    </div>
+    <a href="logout.php">Log Out</a>
+</div>
+
+</body>
+</html>
